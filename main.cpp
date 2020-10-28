a6760ca0528edf93805c749b83682de7bd81688b
Ximena Vega <ximena.garcia7482@alumnos.udg.mx>
2020-10-27 22:29:33 -0500

    use insertar final e inicial

--- main.cpp
+++ main.cpp
@@ -4,14 +4,22 @@ using namespace std;
 
 int main(){
     Arreglo arreglo;
-    arreglo.insertarFinal("hola");
-    arreglo.insertarFinal("mundo");
+    arreglo.insertarFinal("el gato");
+    arreglo.insertarFinal("gris");
+    arreglo.insertarFinal("de alado");
+    arreglo.insertarFinal("del la");
+    arreglo.insertarFinal("calle");
+    arreglo.insertarFinal("está");
+    arreglo.insertarFinal("bonito");
     arreglo.insertarFinal("!");
     arreglo.insertarInicio("¡");
+    arreglo.insertarInicio(":3 ");
+    arreglo.insertarInicio("\n");
 
     for(int i = 0; i < arreglo.size(); i++){
-        cout << arreglo[i] << endl;
+        cout << arreglo[i] << " ";
     }
+    cout << endl;
 
     return 0;
 }
